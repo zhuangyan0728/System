1e9fa83aba34399cd38c9ea7b43cbf84122c429c
Nicolas MAUGER <nicolas.mauger@etudiant.univ-lille1.fr>
2015-11-20 16:42:43 +0100

    update

---
+++ tp7/tp7_1.c
@@ -0,0 +1,39 @@
+/**********************
+* TP N◦ 7 – Impélentation de la commande find
+* Nicolas MAUGER
+* 20/11/15
+***********************/
+#include "tp7_1.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+
+
+/*
+opendir
+closedir
+readdir
+stat
+lstat
+*/
+
+int main(/*int argc, char **argv*/){
+	parcourir_repertoire(".");
+	return 1;
+}
+
+void traiter_fichier(const char *chemin){
+	printf("%s\n", chemin);
+	exit(1);
+}
+
+void parcourir_repertoire(const char *chemin){
+	DIR * folder = opendir(chemin);
+	struct dirent *stuct_dir;
+	while((stuct_dir = readdir(folder)) != NULL){
+		printf("%s\n", stuct_dir->d_name);
+
+	}
+}
---
+++ tp7/tp7_1.h
@@ -0,0 +1,36 @@
+/**********************
+* TP N◦ 7 – Impélentation de la commande find
+* Nicolas MAUGER
+* 20/11/15
+***********************/
+
+enum
+{
+	NAME,
+	TYPE,
+	EXEC,
+	EMPTY,
+	NEWER,
+	INVALID,
+};
+
+typedef struct
+{
+	int type ;
+	union
+	{
+		const char *motif;
+		const char *chemin ;
+		char type_fichier;
+	} operande ;
+} expression ;
+
+void traiter_fichier(const char *chemin);
+void parcourir_repertoire(const char *chemin);
+/*
+int nom_correspond(const char *chemin, const char *motif);
+int type_correspond(const struct stat *buf, char type);
+int executable(const struct stat *buf);
+int vide(const struct stat *buf);
+int plus_recent(const struct stat *buf, const char *fichier_reference);
+*/
